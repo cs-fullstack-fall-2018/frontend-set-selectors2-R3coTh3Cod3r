ba03c214daa9678036fb1412117fe990d8e849db
Kenn <kenn+git@code-crew.org>
2018-08-15 16:37:15 -0500

    Graded 08-15-2018 16:35:46

--- js.js
+++ js.js
@@ -2,3 +2,16 @@ var pID = document.getElementsByTagName("id")
 var pClass = document.getElementsByTagName("class");
 var pTag= document.getElementsByTagName("p");
 console.log(pTag);
+
+/*
+Question 1:
+	a) Call the other two variables in your console.log
+Questoin 2: 
+	a) Not complete.
+Question 3:
+	a) Not complete.
+Question 4:
+	a) Not complete.
+
+From Kenn
+*/
