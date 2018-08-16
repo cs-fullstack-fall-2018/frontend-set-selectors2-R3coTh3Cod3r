c04efa8f8e56d9ef660a8f9320389a3843b53e44
Mareco Nabors <naborsmarico99@gmail.com>
2018-08-16 10:23:33 -0500

    fixing and catching up

--- js.js
+++ js.js
@@ -1,4 +1,20 @@
-var pID = document.getElementsByTagName("id")
-var pClass = document.getElementsByTagName("class");
-var pTag= document.getElementsByTagName("p");
+var pTag = document.getElementById("first");
 console.log(pTag);
+
+var uTag = document.getElementsByClassName("special");
+console.log(uTag);
+
+var vTag = document.getElementsByTagName("p");
+console.log(vTag);
+
+var special = document.querySelector("special");
+console.log(special);
+
+var specicalone = document.querySelectorAll("special");
+console.log(specicalone);
+
+var h1statement = document.getElementsByTagName("h1");
+h1statement[0].style.color="blue";
+var lastpTag = document.getElementById("last");
+console.log(lastpTag);
+lastpTag.style.color="yellow";
